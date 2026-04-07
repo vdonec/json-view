0a24ebbfb61fadbba6257ad8ed5e81beecd52b72
vdonec <vdonec@users.noreply.github.com>
2026-04-07 16:37:21 +0000

    deploy: 6f7a4579b5cb021048a496df14897cd9d0cc0ce2

--- src/utils/getDataType.js
+++
@@ -1,9 +0,0 @@
-/**
- * Get value data type
- * @param {*} val
- */
-export default function getDataType(val) {
-  if (Array.isArray(val)) return 'array';
-  if (val === null) return 'null';
-  return typeof val;
-}
